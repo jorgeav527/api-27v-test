1e0131542b886353f4479a5091a819c994f786e6
jorgeav527 <jorgeav527@gmail.com>
2025-12-30 20:09:25 -0500

    add: list posts

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, jsonify
+import sqlite3
 
 app = Flask(__name__)
 
-# conection
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 my_dict = {
     "nombre": "jorge",
@@ -15,15 +19,21 @@ def json():
     # extraer los datos de la base de datos
     return jsonify(my_dict)
 
-@app.route("/index")
-def index():
+@app.route("/str")
+def string():
+    return "hola con todos"
+
+@app.route("/")
+def base():
     # extraer los datos de la base de datos
-    return render_template("index.html")
+    return render_template("base.html")
 
 @app.route("/home")
 def home():
     return render_template("home.html")
 
-@app.route("/str")
-def string():
-    return "hola con todos"
+@app.route('/post', methods=['GET'])
+def get_all_post():
+    conn = get_db_connection()
+    posts_data = conn.execute('SELECT * FROM posts').fetchall()
+    return render_template("post/list_post.html", posts=posts_data)
